e84765a3627d41a7f17544f486317ab2bb6af9ad
ferupk <feru.pratama@ui.ac.id>
2025-02-28 17:46:06 +0700

    Fixed errors caused by linting fixes

--- scenes/scripts/Stompable.gd
+++ scenes/scripts/Stompable.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D):
-	if body.name == "Player" and (body.isStomping or body.stompBounce):
+	if body.name == "Player" and (body.is_stomping or body.stomp_bounce):
 		self.queue_free()
