f31f21990b2877d65dc51b225e7358e7c112efed
ferupk <feru.pratama@ui.ac.id>
2025-03-14 03:45:12 +0700

    Rework breakable tile with sound effect

--- scenes/scripts/Crystal.gd
+++ scenes/scripts/Crystal.gd
@@ -8,6 +8,7 @@ const FADE_DURATION = 1
 @onready var hum = $AudioStreamPlayer2D
 @onready var fade_out_tween: Tween
 
+
 func _on_area_2d_body_entered(body: Node2D):
 	if body.name == "Player":
 		body.win()
@@ -17,9 +18,10 @@ func _on_area_2d_body_entered(body: Node2D):
 			"change_scene_to_file", "res://scenes/levels/Level%s.tscn" % target_level
 		)
 
+
 func _end_hum():
 	fade_out_tween = create_tween()
 	fade_out_tween.tween_property(hum, "volume_db", -80, FADE_TRANS).set_trans(FADE_TRANS)
-	
+
 	await fade_out_tween.finished
 	hum.stop()
--- scenes/scripts/Stompable.gd
+++ scenes/scripts/Stompable.gd
@@ -1,6 +1,17 @@
 extends Node2D
 
+@onready var break_sound = $AudioStreamPlayer
+
 
 func _on_body_entered(body: Node2D):
 	if body.name == "Player" and (body.is_stomping or body.stomp_bounce):
+		await _break()
 		self.queue_free()
+
+
+func _break():
+	$Sprites.call_deferred("set_visible", false)
+	$CollisionShape2D.call_deferred("set_disabled", true)
+
+	break_sound.play()
+	await break_sound.finished
